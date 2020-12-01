34c9ea7a68b55105b6e9710999fda1ce0dfb730a
deon <deon@zipcodes-MacBook-Pro-23.local>
2020-11-30 21:55:05 -0500

    Finished Dicey Lab

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,24 @@
+import java.util.Random;
+
 public class Dice {
 
+    private Integer numberOfDie;
+    private Random random;
 
+    public Dice(Integer numberOfDie){
+        this.numberOfDie = numberOfDie;
+        this.random = new Random();
+    }
+
+    public void setSeed(Long seed) {
+        random.setSeed(seed);
+    }
+
+    public Integer tossAndSum(){
+        Integer sum = 0;
+        for (int i = 0; i < numberOfDie; i++) {
+            sum += random.nextInt(6) + 1;
+        }
+        return sum;
+    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,24 @@
 public class Simulation {
+    private Integer numberOfDies;
+    private Integer numberOfTosses;
+    private Bins bin;
+
+
+    public Simulation(Integer numberOfDies, Integer numberOfTosses) {
+        this.numberOfDies = numberOfDies;
+        this.numberOfTosses = numberOfTosses;
+        this.bin = new Bins(2, 12);
+
+    }
+
+    public void runSimulation(){
+        Dice dice = new Dice(numberOfDies);
+        for (int i = 0; i < this.numberOfTosses; i++) {
+            Integer rolledNumber = dice.tossAndSum();
+            this.bin.increment(rolledNumber);
 
 
 
+        }
+    }
 }
